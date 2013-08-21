d81cd8bf55fbb560baaab25d442ba67be579c999
Christophe Tournery <christophe.tournery@illusonic.com>
2013-08-21 11:34:16 +0200

    Fix compilation with Visual Studio

--- src/dictionary.h
+++ src/dictionary.h
@@ -21,7 +21,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#ifndef _WIN32
 #include <unistd.h>
+#endif
 
 /*---------------------------------------------------------------------------
                                 New types
