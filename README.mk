ed58ca02ad0595d6bb5088bbff2126012218980f
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2013-10-29 16:40:02 +0100

    Update REAME

---
+++ README.mk
@@ -0,0 +1,30 @@
+# Iniparser 4 #
+
+## 0 - Foreword
+
+[Iniparser4](https://github.com/touilleMan/iniparser4) is a cordial
+fork of [Iniparser](https://github.com/ndevilla/iniparser).
+Iniparser4 is aimed at beeing a new cuting edge version of this
+project while the original Iniparser will be kept as legacy for
+compatibility reasons.
+
+## I - Overview
+
+This modules offers parsing of ini files from the C level.
+See a complete documentation in HTML format, from this directory
+open the file html/index.html with any HTML-capable browser.
+
+## II - Building project
+
+A simple `make` at the root of the project should be enough to get the static
+(i.e. `libiniparser.a`) and shared (i.e. `libiniparser.so.0`) libraries compiled.
+
+You should consider trying the following rules too :
+
+ - `make check` : run the unitary tests
+ - `make exemples` : compile the exemples
+
+## III - License
+
+This software is released under MIT License.
+See LICENSE for full informations
