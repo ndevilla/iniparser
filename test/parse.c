5168bcb29cbe8f694ca73d41e3fbb55cd1296495
chin-tse <zhoujinze@gmail.com>
2014-11-11 17:23:31 +0800

    add test

--- test/parse.c
+++ test/parse.c
@@ -14,11 +14,18 @@ int main(int argc, char * argv[])
     char       * ini_name ;
     char       * val;
     int         rv;
+    FILE       *out_fd;
 
     if (argc<2) {
         ini_name = "twisted.ini";
     } else {
         ini_name = argv[1] ;
+    }
+
+    out_fd = fopen("./parse.out.ini", "w");
+    if (!out_fd) {
+        printf("Error:open output file fail!\n");
+        return 0;
     }
 
     ini = iniparser_load(ini_name);
@@ -34,6 +41,10 @@ int main(int argc, char * argv[])
 
     val = iniparser_getstring(ini, "log-format:test:test", def);
     printf("Get test:%s\n", val);
+    iniparser_dump_ini(ini, out_fd);
+    fclose(out_fd);
+
+    
 
     iniparser_freedict(ini);
 
