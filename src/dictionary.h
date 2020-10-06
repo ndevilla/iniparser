a8f0fa0d582af1c69d0acbc6cbe5192d5d3aca02
Tim Gates <tim.gates@iress.com>
2020-10-06 22:03:49 +1100

    docs: fix simple typo, objet -> object
    There is a small typo in src/dictionary.c, src/dictionary.h.
    
    Should read `object` rather than `objet`.

--- src/dictionary.h
+++ src/dictionary.h
@@ -73,7 +73,7 @@ unsigned dictionary_hash(const char * key);
 /**
   @brief    Create a new dictionary object.
   @param    size    Optional initial size of the dictionary.
-  @return   1 newly allocated dictionary objet.
+  @return   1 newly allocated dictionary object.
 
   This function allocates a new dictionary object of given size and returns
   it. If you do not know in advance (roughly) the number of entries in the
